8c4c5e49a8aa1715cfa36e365685bf452182f903
Igor <igor.djurdjic89@gmail.com>
2022-02-05 19:33:07 +0100

    Update Database.php

--- app/src/Database.php
+++ app/src/Database.php
@@ -47,6 +47,7 @@ class Database {
             $this->pdo = new \PDO($dsn, $this->user, $this->pass);
             $this->pdo->setAttribute(\PDO::ATTR_ERRMODE,            \PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+            $this->pdo->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
             $this->pdo->setAttribute(\PDO::ATTR_PERSISTENT,         true);
             $this->pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES,   false);
 
