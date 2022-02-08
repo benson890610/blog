977de77f8f47a7305f370342f469009679c32ac5
Igor <igor.djurdjic89@gmail.com>
2022-02-08 10:48:03 +0100

    Update db.php

--- app/app/config/db.php
+++ app/app/config/db.php
@@ -1,11 +1,11 @@
 <?php 
 
 /*
-define('DB_DRIVER', "mysql");
-define('DB_HOST', "localhost");
-define('DB_USER', "root");
-define('DB_PASS', "benson890610");
-define('DB_NAME', "blog");
-define('DB_PORT', 3306);
-define('DB_CHAR', "utf8mb4");
+define('DB_DRIVER', "__DB_DRIVER__");
+define('DB_HOST', "__DB_HOST__");
+define('DB_USER', "__DB_USER__");
+define('DB_PASS', "__DB_PASSWORD__");
+define('DB_NAME', "__DB_NAME__");
+define('DB_PORT', "__DB_PORT__");
+define('DB_CHAR', "__DB_CHARACTER_SET");
 */
